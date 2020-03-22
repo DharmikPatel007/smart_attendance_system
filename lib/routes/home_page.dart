8a8df793aa08352f8dc11f9f15ad92f173aa3610
DharmikPatel007 <dharmikpatel007.dp@gmail.com>
2020-03-22 15:29:25 +0530

    Only About Page is Remaining !

--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -7,6 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../routes/root_page.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import '../drawer/take_attendance/take_attendance.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -18,7 +19,21 @@ class _HomePageState extends State<HomePage> {
   bool progressIndicator = false;
   String _loginName = 'name';
   String _loginEmail = 'email';
+  List<Widget> imageList = [];
 
+  _signOut() async {
+    setState(() {
+      progressIndicator = true;
+    });
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('is_logged_in', false);
+    await Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacementNamed(RootPage.id);
+    });
+    setState(() {
+      progressIndicator = false;
+    });
+  }
   @override
   void initState() {
     super.initState();
@@ -28,6 +43,12 @@ class _HomePageState extends State<HomePage> {
         _loginEmail = onValue[1];
       });
     });
+    for(int i=0; i<6; i++){
+      imageList.add(Image.asset('images/carousel/c$i.jpg',
+        fit: BoxFit.fill,
+        width: 300,
+      ));
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -38,6 +59,7 @@ class _HomePageState extends State<HomePage> {
           title: Text('Smart Attendance System'),
         ),
         drawer: Drawer(
+          elevation: 5,
           child: ListView(children: <Widget>[
             UserAccountsDrawerHeader(
               accountName: Text(_loginName),
@@ -74,28 +96,82 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               title: Text('Sign Out'),
               trailing: Icon(CupertinoIcons.reply_thick_solid),
-              onTap: () async {
-                setState(() {
-                  progressIndicator = true;
-                });
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                prefs.setBool('is_logged_in', false);
-                await Future.delayed(Duration(seconds: 2), () {
-                  Navigator.of(context).pushReplacementNamed(RootPage.id);
-                });
-                setState(() {
-                  progressIndicator = false;
-                });
-              },
+              onTap: _signOut,
             )
           ]),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Welcome To Smart Attendance System !'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Flexible(
+              child: CarouselSlider(
+                autoPlay: true,
+                items: imageList,
+                enlargeCenterPage: true,
+                pauseAutoPlayOnTouch: Duration(seconds: 1),
+                height: 375,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ReusableCard(cardLabel: 'Take Attendance',iconData: CupertinoIcons.create_solid,routeName: AttendancePage.id,),
+                ReusableCard(cardLabel: 'Manage Students',iconData: CupertinoIcons.gear_solid,routeName: ManageStudentsPage.id,),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ReusableCard(cardLabel: 'About US',iconData: CupertinoIcons.group_solid,routeName: AboutPage.id,),
+                ReusableCard(cardLabel: 'Sign Out',iconData: CupertinoIcons.reply_thick_solid,onTouch: _signOut,),
+              ],
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final IconData iconData;
+  final String cardLabel;
+  final String routeName;
+  final Function onTouch;
+  ReusableCard({@required this.iconData,@required this.cardLabel,this.routeName,this.onTouch});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: 190,
+        child: Card(
+          color: Colors.black38,
+          elevation: 5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(iconData,size: 125,),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(cardLabel,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
+      onTap: routeName != null ? (){
+        Navigator.of(context).pushNamed(routeName);
+      } : onTouch,
     );
   }
 }
